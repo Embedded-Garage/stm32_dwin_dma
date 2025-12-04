1efe2b394c8f10ce1d1ae2962309c063b4a1c83c
Embedded Garage <embedded.garage.yt@gmail.com>
2025-12-04 15:38:30 +0100

    6# Read command parsing

--- App/dwin_comm.c
+++ App/dwin_comm.c
@@ -2,11 +2,15 @@
 
 #include "stm32g4xx_hal.h"
 #include "stm32g4xx_hal_uart.h"
+#include <stdio.h>
 
 extern UART_HandleTypeDef huart2;
 
 #define DWIN_SOF1 0x5Au
 #define DWIN_SOF2 0xA5u
+
+#define DWIN_COMMAND_READ 0x83u
+#define DWIN_COMMAND_READ_HEADER_LENGTH 3u
 
 #define DWIN_MAX_DATA_LENGTH 255u
 
@@ -27,6 +31,8 @@ typedef struct {
 static dwin_comm_ctx_s ctx = {.parser_state = PARSE_STATE_SOF1};
 
 static void parse_rx_data(uint8_t *data, uint8_t len);
+static void parse_read_command(uint8_t *payload, uint8_t payload_length);
+static void handle_read_vp(uint16_t address, uint16_t value);
 
 void dwin_comm_parse(uint8_t character) {
   switch (ctx.parser_state) {
@@ -50,10 +56,9 @@ void dwin_comm_parse(uint8_t character) {
 
   case PARSE_STATE_DATA:
     ctx.data[ctx.data_index++] = character;
-    if (ctx.data_index >= ctx.length)
-    {
-        parse_rx_data(ctx.data, ctx.length);
-        ctx.parser_state = PARSE_STATE_SOF1;
+    if (ctx.data_index >= ctx.length) {
+      parse_rx_data(ctx.data, ctx.length);
+      ctx.parser_state = PARSE_STATE_SOF1;
     }
     break;
 
@@ -62,7 +67,56 @@ void dwin_comm_parse(uint8_t character) {
   }
 }
 
-static void parse_rx_data(uint8_t *data, uint8_t len)
+static void parse_rx_data(uint8_t *data, uint8_t len) {
+  if (len < 1u)
+    return;
+
+  uint8_t command = data[0u];
+  uint8_t *payload = &data[1u];
+  uint8_t payload_length = len - 1u;
+
+  switch (command) {
+  case DWIN_COMMAND_READ:
+    parse_read_command(payload, payload_length);
+    break;
+
+  default:
+    /* Unknown command */
+    break;
+  }
+}
+
+static void parse_read_command(uint8_t *payload, uint8_t payload_length) {
+  if (payload_length < DWIN_COMMAND_READ_HEADER_LENGTH)
+    return;
+
+  uint16_t starting_address = (payload[0] << 8) | payload[1];
+  uint8_t vp_cnt = payload[2];
+
+  uint8_t vp_bytes = vp_cnt * 2u;
+  uint8_t data_length = payload_length - DWIN_COMMAND_READ_HEADER_LENGTH;
+
+  if (vp_bytes != data_length)
+    return;
+
+  uint8_t *data_ptr = &payload[DWIN_COMMAND_READ_HEADER_LENGTH];
+
+  for (uint8_t i = 0; i < vp_cnt; i++) {
+    uint16_t address = starting_address + i;
+
+    // uint16_t value = data_ptr[i * 2] << 8 | data_ptr[i * 2 + 1];
+
+    uint16_t value = (data_ptr[0u] << 8) | data_ptr[1u];
+    data_ptr += 2u;
+
+    handle_read_vp(address, value);
+  }
+}
+
+static void handle_read_vp(uint16_t address, uint16_t value)
 {
-    HAL_UART_Transmit(&huart2, (const uint8_t *)"dwin data received", 18, HAL_MAX_DELAY);
+  char tmp_buf[64u];
+  size_t tmp_len = sprintf(tmp_buf, "Address: 0x%04X, Value: 0x%04X\r\n", address, value);
+
+  HAL_UART_Transmit(&huart2, (uint8_t *)tmp_buf, tmp_len, HAL_MAX_DELAY);
 }
